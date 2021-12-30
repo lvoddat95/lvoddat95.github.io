c3d1b9388982fa78c43cf74dc9dfa4e3a95a56d5
Dat Lv <lvoddat95@yahoo.com>
2021-12-30 08:39:10 +0700

    update

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
-    
+
     function setClasses(index, steps) {
         if (index < 0 || index > steps) return;
         if (index == 0) {
-            $("#prev").prop('disabled', true).css({"visibility": "hidden", "opacity": "0"});
+            $("#prev").prop('disabled', true).css({
+                "visibility": "hidden",
+                "opacity": "0"
+            });
         } else {
-            $("#prev").prop('disabled', false).css({"visibility": "visible", "opacity": "1"});
+            $("#prev").prop('disabled', false).css({
+                "visibility": "visible",
+                "opacity": "1"
+            });
         }
         if (index == steps) {
-            $("#next").text('Hoàn thành');
+            $("#next").html('Hoàn thành');
         } else {
-            $("#next").text('Tiếp tục');
+            $("#next").html('Tiếp tục <i class="far fa-fw fa-long-arrow-right"></i>');
         }
         $(".step-wizard ul li").each(function () {
             $(this).removeClass();
@@ -31,7 +37,7 @@ $(document).ready(function () {
         e.preventDefault();
         var step = $(this).find("div.step-number")[0].innerText;
         var steps = $(".step-wizard ul li").length;
-        setClasses(step - 1, steps - 1)
+        setClasses(step - 1, steps - 1);
     });
 
     $("#prev").click(function (e) {
@@ -39,6 +45,7 @@ $(document).ready(function () {
         var step = $(".step-wizard ul li.active div.step-number")[0].innerText;
         var steps = $(".step-wizard ul li").length;
         setClasses(step - 2, steps - 1);
+        scroll_to_element( $('.step-nav'), 700 );
     });
 
     $("#next").click(function (e) {
@@ -50,8 +57,22 @@ $(document).ready(function () {
             var steps = $(".step-wizard ul li").length;
             setClasses(step, steps - 1);
         }
+        console.log($(this));
+        scroll_to_element( $('.step-nav'), 700 );
     });
 
     // initial state setup
     setClasses(0, $(".step-wizard ul li").length);
+
+    // $(".btn-mua").click(function (e) {
+    //     e.preventDefault();
+    //     scroll_to_element( $('#entry__form'), 700 );
+    // });
 });
+
+var scroll_to_element = function(el, ms){
+    var speed = (ms) ? ms : 600;
+    $('html,body').animate({
+        scrollTop: $(el).offset().top
+    }, speed);
+}
