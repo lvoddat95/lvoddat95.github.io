4b6eabdba0c5ab13d6dedd5a7f9f884dc98bbc93
Dat Lv <lvoddat95@yahoo.com>
2022-04-03 16:12:46 +0700

    update cancer insurance

---
+++ cancer-insurance/src/assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  // $(".owl-carousel").owlCarousel({
+  //   items: 4,
+  //   loop: false,
+  //   margin: 0,
+  //   nav: true,
+  // });
+
+
+  $('.ci-package-slider').slick({
+    infinite: false,
+    slidesToShow: 3,
+  });
+});
